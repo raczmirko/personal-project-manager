1ea7e41e8a2880268e9701a07174d301f56d0a83
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-05-08 10:50:21 +0200

    feat: added ConnectionController class

---
+++ src/main/java/hu/okrim/personalprojectmanager/ConnectionController.java
@@ -0,0 +1,16 @@
+package hu.okrim.personalprojectmanager;
+
+public class ConnectionController {
+    public static String getConnectionURLSS(String server, String db, String user, String password){
+        String conn = "jdbc:sqlserver://" + server + ";"
+                        + "database=" + db + ";"
+                        + "user=" + user + ";"
+                        + "password=" +password + ";"
+                        + "encrypt=true;"
+                        + "trustServerCertificate=true;"
+                        + "loginTimeout=30;";
+        return conn;
+    }
+
+
+}
