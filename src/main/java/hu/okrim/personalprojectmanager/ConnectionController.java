e61fceb21d86ff8336e0215d053da0b0f877acc6
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-05-09 14:46:38 +0200

    fixed: reduced redundancy by changing return to inline return

--- src/main/java/hu/okrim/personalprojectmanager/ConnectionController.java
+++ src/main/java/hu/okrim/personalprojectmanager/ConnectionController.java
@@ -4,19 +4,16 @@ import java.sql.*;
 
 public class ConnectionController {
     public static String getConnectionURLSS(String server, String db, String user, String password){
-        String conn = "jdbc:sqlserver://" + server + ";"
+        return "jdbc:sqlserver://" + server + ";"
                         + "database=" + db + ";"
                         + "user=" + user + ";"
                         + "password=" + password + ";"
                         + "encrypt=true;"
                         + "trustServerCertificate=true;"
                         + "loginTimeout=30;";
-        return conn;
     }
 
     public static Connection establishConnection(String URL) throws SQLException{
         return DriverManager.getConnection(URL);
     }
-
-
 }
