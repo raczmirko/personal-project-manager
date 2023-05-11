5f79eeb4f59dd42e19caf5906526c54eac764022
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-05-11 16:01:29 +0200

    feat: loading popup screen FXML file and its controller class responsible for the progress bar animation

---
+++ src/main/java/hu/okrim/personalprojectmanager/LoadingController.java
@@ -0,0 +1,46 @@
+package hu.okrim.personalprojectmanager;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoadingController implements Initializable {
+    @FXML
+    public ProgressBar progressbar;
+    private Thread progressThread;
+    private static boolean isRunning;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        isRunning = true;
+    }
+
+    public void startProgressBarThread() {
+        progressThread = new Thread(() -> {
+            while (isRunning) {
+                for (double progress = 0; progress <= 1.0; progress += 0.01) {
+                    double finalProgress = progress;
+                    Platform.runLater(() -> progressbar.setProgress(finalProgress));
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                }
+            }
+        });
+
+        progressThread.setDaemon(true);
+        progressThread.start();
+    }
+
+    public void stopProgressBarThread() {
+        isRunning = false;
+        progressThread.interrupt();
+    }
+}
