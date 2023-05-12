e6e1c62a4fb16509206ea516ee116a5056cbcc6b
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-05-12 21:15:31 +0200

    feat: app icon is now set upon launch

--- src/main/java/hu/okrim/personalprojectmanager/ManagerApplication.java
+++ src/main/java/hu/okrim/personalprojectmanager/ManagerApplication.java
@@ -3,9 +3,11 @@ package hu.okrim.personalprojectmanager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class ManagerApplication extends Application {
     @Override
@@ -13,6 +15,13 @@ public class ManagerApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(ManagerApplication.class.getResource("manager-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("Personal Project Manager");
+        // Set the application icon if it exists
+        InputStream iconStream = getClass().getResourceAsStream("icon.png");
+        Image icon;
+        if (iconStream != null) {
+            icon = new Image(iconStream);
+            stage.getIcons().add(icon);
+        }
         stage.setScene(scene);
         stage.show();
     }
