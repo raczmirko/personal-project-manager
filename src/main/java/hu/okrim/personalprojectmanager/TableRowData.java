0ec971bdd47cd1ad00c78641007934f584de46ae
raczmirko <123543210+raczmirko@users.noreply.github.com>
2023-05-15 13:46:41 +0200

    refactor: removed unnecessary white spaces

--- src/main/java/hu/okrim/personalprojectmanager/TableRowData.java
+++ src/main/java/hu/okrim/personalprojectmanager/TableRowData.java
@@ -2,21 +2,17 @@ package hu.okrim.personalprojectmanager;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class TableRowData {
     private final List<StringProperty> rowData = new ArrayList<>();
-
     public void addColumnData(String columnData) {
         rowData.add(new SimpleStringProperty(columnData));
     }
-
     public StringProperty getColumnData(int columnIndex) {
         return rowData.get(columnIndex);
     }
-
     public List<String> getAllDataFromRow(){
         List<String> rowData = new ArrayList<>();
         for(StringProperty str : this.rowData){
@@ -24,9 +20,7 @@ public class TableRowData {
         }
         return rowData;
     }
-
     public void setColumnData(int columnIndex, String newValue) {
         getColumnData(columnIndex).set(newValue);
     }
 }
-
